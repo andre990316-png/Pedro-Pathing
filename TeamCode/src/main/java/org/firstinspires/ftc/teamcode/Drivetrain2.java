2fcd01edfa62f355442bc2fa5c1bb657b2e628f2
RyanH-0 <sckgfchsyf@privaterelay.appleid.com>
2026-01-10 18:15:21 +0800

    increased targetrpm adjust rate

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain2.java
@@ -175,12 +175,9 @@ public class Drivetrain2 extends OpMode {
         // ---------------------------
 
         // Increase / decrease target RPM while holding
-        double rpmAdjustRate = 400;   // RPM per second
 
-        if (gamepad2.dpad_up && !DpadUpPrev)   targetRPM += rpmAdjustRate * Shooter_dt;
-        if (gamepad2.dpad_down && !DpadDownPrev) targetRPM -= rpmAdjustRate * Shooter_dt;
-        DpadUpPrev = gamepad2.dpad_up;
-        DpadDownPrev = gamepad2.dpad_down;
+        if (gamepad2.dpad_up && !DpadUpPrev)   targetRPM = targetRPM + 100;
+        if (gamepad2.dpad_down && !DpadDownPrev) targetRPM = targetRPM - 100;
         targetRPM = Range.clip(targetRPM, 0, 6000);
 
         double error = targetRPM - rpm;
