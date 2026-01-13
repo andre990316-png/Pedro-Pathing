ae87f804ec0ef192078e8e1ce0773fe8faa9e660
errorOnLine168 <benmango13@gmail.com>
2026-01-13 19:11:58 +0800

    capital M

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/FlywheelLogic.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/FlywheelLogic.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.mechanisms;
+package org.firstinspires.ftc.teamcode.Mechanisms;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
