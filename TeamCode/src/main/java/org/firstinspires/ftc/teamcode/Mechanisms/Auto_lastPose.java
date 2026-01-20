a16d2f5a1da77a67222c6a75a139e75aeba68be8
RyanH-0 <6r7v6dbnw4@privaterelay.appleid.com>
2026-01-20 16:04:26 +0800

    Added Auto_lastPose.java to save the position at which autonomous ended in order to continue using the pinpoint position inside Teleop in the future.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Auto_lastPose.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.Mechanisms;
+
+import com.pedropathing.geometry.Pose;
+
+public class Auto_lastPose {
+    public static Pose currentPose = new Pose(0, 0, 0);
+}
