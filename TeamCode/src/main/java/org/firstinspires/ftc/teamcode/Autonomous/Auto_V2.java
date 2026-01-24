93362bd4bc9367dc96542ae7f06a662ef5112097
errorOnLine168 <benmango13@gmail.com>
2026-01-24 15:42:09 +0800

    fixed bug...?.....

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Auto_V2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Auto_V2.java
@@ -430,6 +430,7 @@ public class Auto_V2 extends OpMode {
         }else{
             String[] names = {"top left","top right","bottom left","bottom right"};
             ArrayList<ArrayList<AutoStep>> paths = new ArrayList<>();
+            buildSteps();
             paths.add(AUTOTOPLEFT2);
             paths.add(AUTOTOPRIGHT2);
             paths.add(AUTOBOTTOMLEFT);
