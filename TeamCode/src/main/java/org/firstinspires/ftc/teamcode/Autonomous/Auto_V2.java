bc32890d33c68159c535621a4c7921feae498133
errorOnLine168 <benmango13@gmail.com>
2026-01-25 14:03:46 +0800

    idk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Auto_V2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Auto_V2.java
@@ -420,7 +420,7 @@ public class Auto_V2 extends OpMode {
         }else{
             String[] names = {"top left","top right","bottom left","bottom right"};
             ArrayList<ArrayList<AutoStep>> paths = new ArrayList<>();
-            buildSteps();
+            //buildSteps();
             paths.add(AUTOTOPLEFT);
             paths.add(AUTOTOPRIGHT2);
             paths.add(AUTOBOTTOMLEFT);
