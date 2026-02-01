13e8bfdf99f82ca408a26e9275c21ef9c20011d1
errorOnLine168 <benmango13@gmail.com>
2026-02-01 15:34:17 +0800

    AUTO SELECTION FIXED!!!!!! WHOOOOO

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Auto_V2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Auto_V2.java
@@ -316,18 +316,20 @@ public class Auto_V2 extends OpMode {
         if (AllianceData.isRed()) {
 
 
-            STEPS.addAll(AUTOTOPRIGHT);
+            STEPS.addAll(AUTOTOPRIGHT2);
 
             telemetry.addData("Auto Path", "RED (Right Side)");
+            limelight.pipelineSwitch(LimelightAim.pipelineFromName("Red"));
         } else {
 
-            STEPS.addAll(AUTOTOPLEFT);
+            STEPS.addAll(AUTOTOPLEFT2);
 
             telemetry.addData("Auto Path", "BLUE (Left Side)");
+            limelight.pipelineSwitch(LimelightAim.pipelineFromName("Blue"));
         }
 
-        paths.add(AUTOTOPLEFT);
-        paths.add(AUTOTOPRIGHT);
+        paths.add(AUTOTOPLEFT4);
+        paths.add(AUTOTOPRIGHT4);
         paths.add(AUTOBOTTOMLEFT);
         paths.add(AUTOBOTTOMRIGHT);
 
@@ -472,6 +474,7 @@ public class Auto_V2 extends OpMode {
 
     @Override
     public void init() {
+        limelight = hardwareMap.get(Limelight3A.class, "Limelight");
         pathTimer = new Timer();
         opModeTimer = new Timer();
 
@@ -482,8 +485,8 @@ public class Auto_V2 extends OpMode {
         buildSteps();
         //STEPS.addAll(AUTOTOPLEFT);
 
-        Pose start = (!STEPS.isEmpty() && STEPS.get(0).pose != null) ? STEPS.get(0).pose : topLeftStartPose;
-        follower.setStartingPose(start);   // recommended for Pedro
+        //Pose start = STEPS.get(0).pose;
+        //follower.setStartingPose(start);   // recommended for Pedro
 
 
         buildChainsFromSteps();
@@ -504,7 +507,7 @@ public class Auto_V2 extends OpMode {
         );
         imu.initialize(new IMU.Parameters(revHubOrientationOnRobot));
 
-        limelight = hardwareMap.get(Limelight3A.class, "Limelight");
+
         if (AllianceData.isRed()) {
             limelight.pipelineSwitch(LimelightAim.pipelineFromName("Red"));
             telemetry.addData("Alliance", "RED (Pipeline set to Red)");
@@ -637,6 +640,23 @@ public class Auto_V2 extends OpMode {
         telemetry.addData("y", follower.getPose().getY());
         telemetry.addData("heading", follower.getPose().getHeading());
         telemetry.addData("stuckon", stuckon);
+        if (currentIndex < STEPS.size()) {
+            Pose currentPose = follower.getPose();
+            Pose nextPose = STEPS.get(currentIndex).pose;
+
+            if (nextPose != null) {
+                double dx2 = nextPose.getX() - currentPose.getX();
+                double dy2 = nextPose.getY() - currentPose.getY();
+                double distanceToNext = Math.hypot(dx2, dy2);
+
+                telemetry.addData("Distance to Next Pose", "%.2f", distanceToNext);
+                telemetry.addData("Next Pose X/Y", "%.2f / %.2f", nextPose.getX(), nextPose.getY());
+            } else {
+                telemetry.addData("Distance to Next Pose", "No target (null pose)");
+            }
+        } else {
+            telemetry.addData("Distance to Next Pose", "DONE");
+        }
         telemetry.update();
     }
 }
