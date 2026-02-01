f0020515b26f79daf487b874a112f9216b54d283
errorOnLine168 <benmango13@gmail.com>
2026-02-01 16:55:07 +0800

    thing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Auto_V2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Auto_V2.java
@@ -131,7 +131,7 @@ public class Auto_V2 extends OpMode {
     private final Pose redLoadingZoneEnd = new Pose(133.8,10, Math.toRadians(0));
 
     //gate and intake poses
-    private final Pose blueGateIntakePose = new Pose(11,55, Math.toRadians(130));
+    private final Pose blueGateIntakePose = new Pose(13,54, Math.toRadians(140));
     private final Pose redGateIntakePose = new Pose(11,62, Math.toRadians(120));
 
 
